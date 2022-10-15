e995486dc670e388260a54555e45f1462ac87f67
luizdaniel233 <luizsdossantos@icomp.ufam.edu.br>
2022-10-15 19:23:05 -0400

    AJD-X:Create Prisma Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './database/PrismaService';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,PrismaService],
 })
 export class AppModule {}
