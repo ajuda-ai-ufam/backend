a70c9228e0de26cb109a4b058b2d35e4713733f6
luizdaniel233 <luizsdossantos@icomp.ufam.edu.br>
2022-10-16 20:24:52 -0400

    AJD-X:Create Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger'
+
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ajudaai API')
+    .setDescription('Backend do Ajudaai')
+    .setVersion('1.0')
+    .addTag('Ajudaai')
+    .addBearerAuth(
+      {
+        // I was also testing it without prefix 'Bearer ' before the JWT
+        description: `[just text field] Please enter token in following format: Bearer <JWT>`,
+        name: 'Authorization',
+        bearerFormat: 'Bearer', // I`ve tested not to use this field, but the result was the same
+        scheme: 'Bearer',
+        type: 'http', // I`ve attempted type: 'apiKey' too
+        in: 'Header',
+      },
+      'access-token',
+    )
+    .build();
+
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/swagger', app, document);
+
   await app.listen(process.env.PORT);
+
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 
+@ApiTags('Users')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+
 }
