418622668e5041c0948da7231a46f15cc3026b3e
luizdaniel233 <luizsdossantos@icomp.ufam.edu.br>
2022-10-25 11:04:48 -0400

    AJD-X:Ident

--- prisma/migrations/20221025010241_update_table_student/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `description` to the `student` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `student` ADD COLUMN `description` VARCHAR(500) NOT NULL;
