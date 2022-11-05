7095af8e84d411057c0a3ca964d7c8b9aef50d6b
Igor Vinicius <ivfernandes05@gmail.com>
2022-11-05 16:34:34 -0400

    Enable Cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Super Monitoria API')
     .setDescription('Backend Super Monitoria')
