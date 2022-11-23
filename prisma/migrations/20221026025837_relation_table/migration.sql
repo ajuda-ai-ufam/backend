584e5dcb19d1cbb2cac8c5b90a22114d83fb7866
luizdaniel233 <luizsdossantos@icomp.ufam.edu.br>
2022-11-22 23:08:13 -0400

    BACK-X:Update roles to create student

--- prisma/migrations/20221026025837_relation_table/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `user_type_user_id_fkey` ON `user`;
-
--- AddForeignKey
-ALTER TABLE `user` ADD CONSTRAINT `user_type_user_id_fkey` FOREIGN KEY (`type_user_id`) REFERENCES `type_user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20221119053117_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `schedule_monitoring` MODIFY `id_status` INTEGER NOT NULL DEFAULT 1;
