00fef312fe8195dc001aeb4ca7b7aa5e169754a5
Igor Vinicius <ivfernandes05@gmail.com>
2022-12-01 14:56:06 -0400

    fix: identation

--- src/generate-code/generate-code.controller.ts
+++ src/generate-code/generate-code.controller.ts
@@ -5,26 +5,28 @@ import { VerifyCodeDTO } from './dto/verify-code.dto';
 import { GenerateCodeService } from './generate-code.service';
 
 @Controller('code')
-@ApiTags("Code")
-
+@ApiTags('Code')
 export class GenerateCodeController {
   constructor(private readonly generateCodeService: GenerateCodeService) {}
 
-  @ApiOperation({description:"Rota para listar todos os type-codes."})
-  @Get("type-code")
-  async typeCode(){
+  @ApiOperation({ description: 'Rota para listar todos os type-codes.' })
+  @Get('type-code')
+  async typeCode() {
     return this.generateCodeService.typeCode();
   }
 
-  @ApiOperation({description:"Rota para gerar o código de verificação do email ou de senha."})
-  @Post("generate")
-  async generateCode(@Body() data : CodeDTO){
+  @ApiOperation({
+    description:
+      'Rota para gerar o código de verificação do email ou de senha.',
+  })
+  @Post('generate')
+  async generateCode(@Body() data: CodeDTO) {
     return this.generateCodeService.generate(data);
   }
 
-  @ApiOperation({description:"Rota para validar o código gerado."})
-  @Post("verify")
-  async verifyCode(@Body() data: VerifyCodeDTO){
+  @ApiOperation({ description: 'Rota para validar o código gerado.' })
+  @Post('verify')
+  async verifyCode(@Body() data: VerifyCodeDTO) {
     return this.generateCodeService.verifyCode(data);
   }
 }
