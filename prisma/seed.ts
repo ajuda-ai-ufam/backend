9026fc6572b151360ee5c91324132644a051e31c
luizdaniel233 <luizsdossantos@icomp.ufam.edu.br>
2022-12-21 20:15:59 -0400

    BACK-X:Merge in test

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -72,7 +72,6 @@ async function main() {
   }
 
   for (const subject of subjects_cc) {
-<<<<<<< HEAD
     subject.name = subject.name
       .split(' ')
       .map((word) => {
@@ -81,8 +80,6 @@ async function main() {
         else return word.toLowerCase();
       })
       .join(' ');
-=======
->>>>>>> f5ee865 (Development (#19))
     await prisma.subject.upsert({
       create: { ...subject, course_id: 2 },
       update: { ...subject, course_id: 2 },
