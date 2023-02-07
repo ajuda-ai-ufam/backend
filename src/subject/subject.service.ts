3b40da2e1f7b188c7b3cb5a6d601bfcf50a321a5
Luiz Daniel Marinho dos Santos <62406091+luizdaniel233@users.noreply.github.com>
2023-02-07 12:06:11 -0400

    Development (#37)
    * BACK-87: Add pagination and user name (#35)
    
    * BACK-87: Add pagination and user name
    
    * BACK-87: Add filters on schedules route
    
    * fix: Trata quando nenhum status é passado
    
    * BACK-87: Add subject to schedules route return
    
    * fix: Conserta checagem de permissão
    
    * feat: Cria rota para recusar agendamento
    
    * Back-x:Added file env.example
    
    * BACK-X:Added in query or to available time monitor
    
    * BACK-X:Added in query or to available time monitor
    
    * fix: Recebe ID do monitor para pegar horarios
    
    * fix: Recebe ID do monitor para agendar
    
    * feat: Retorna monitores na lista de disciplinas
    
    * BACK-X:Update rule to refuse monitor
    
    * BACK-X:Update if to list subjects
    
    ---------
    
    Co-authored-by: Nabson Paiva <nabson.paiva@meliuz.com.br>

--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -56,7 +56,8 @@ export class SubjectService {
 
     //somente monitores disponíveis
     data.Monitor.forEach((element) => {
-      if (element.status.id == 3) approved_Monitores.push(element);
+      if (element.status.id == 3 || element.status.id == 2)
+        approved_Monitores.push(element);
     });
 
     data.SubjectResponsability = approved_SubjectResponsability;
