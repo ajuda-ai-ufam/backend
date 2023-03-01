e06ddf1cdbe6b0c8df7a0e07638792fb51d46820
Nabson Paiva <nabson.paiva@meliuz.com.br>
2023-02-28 20:23:22 -0400

    fix: Conserta guard de autorização

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,11 +1,18 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 import { Role } from '../enums/role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector: Reflector, private jwtService: JwtService) {}
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
@@ -15,7 +22,29 @@ export class RolesGuard implements CanActivate {
     if (!requiredRoles) {
       return true;
     }
-    const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles?.includes(role));
+
+    const authToken = context.switchToHttp().getRequest().headers.authorization;
+    if (!authToken) {
+      throw new ForbiddenException({
+        error: {
+          message: 'Não foi encontrado um token de autenticação válido.',
+        },
+      });
+    }
+
+    const payload = this.jwtService.decode(authToken.replace('Bearer ', ''));
+    if (
+      !requiredRoles.some(
+        (role) => role === (payload['type_user']['type'] as unknown as Role),
+      )
+    ) {
+      throw new UnauthorizedException({
+        error: {
+          message: 'Você não tem autorização para performar esta ação.',
+        },
+      });
+    }
+
+    return true;
   }
 }
