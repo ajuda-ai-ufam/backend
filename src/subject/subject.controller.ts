971f1b511aa019173f6d56faf9652fb2142763f5
Jorge Frota <jorgepabloafrota@gmail.com>
2023-04-15 20:31:35 -0400

    feat: Add end responsability route in API

--- src/subject/subject.controller.ts
+++ src/subject/subject.controller.ts
@@ -1,15 +1,37 @@
-import { SubjectDTO } from './dto/subject.dto';
-import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Patch,
+  PreconditionFailedException,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { QueryPaginationDto } from 'src/common/dto/query-pagination.dto';
 import { IResponsePaginate } from 'src/common/interfaces/pagination.interface';
 import { SubjectService } from './subject.service';
+import { EndResponsabilityCommand } from './commands/end-responsability-command';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/enums/role.enum';
+import {
+  AlreadyFinishedException,
+  BlockingMonitorsException,
+  ResponsabilityNotFoundException,
+} from './utils/exceptions';
 
 @Controller('subject')
 @ApiTags('Subjects')
 export class SubjectController {
-  constructor(private readonly subjectService: SubjectService) {}
+  constructor(
+    private readonly subjectService: SubjectService,
+    private readonly endResponsabilityCommand: EndResponsabilityCommand,
+  ) {}
+
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   @ApiOperation({ description: 'Rota para listar todas as disciplinas.' })
@@ -19,10 +41,38 @@ export class SubjectController {
   ): Promise<IResponsePaginate> {
     return await this.subjectService.findAll(query);
   }
+
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   @Get(':id')
   async findOne(@Param('id') id: number) {
     return await this.subjectService.listSubjects(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @Roles(Role.Coordinator)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Atualiza o status de uma responsabilidade para 3 (finalizada)',
+  })
+  @Patch('responsability/:id/end')
+  async endResponsability(@Param('id') id: number) {
+    try {
+      return await this.endResponsabilityCommand.execute(id);
+    } catch (error) {
+      if (error instanceof ResponsabilityNotFoundException) {
+        throw new NotFoundException(error.message);
+      }
+
+      if (
+        error instanceof AlreadyFinishedException ||
+        error instanceof BlockingMonitorsException
+      ) {
+        throw new PreconditionFailedException(error.message);
+      }
+
+      throw error;
+    }
   }
 }
