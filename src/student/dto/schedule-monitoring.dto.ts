b96a27f8db487e9d5b2dafb61e96c660f0115fbd
Nabson Paiva <nabson.paiva@meliuz.com.br>
2023-04-16 17:03:32 -0400

    fix: Valida disponibilidade do monitor ao agendar

--- src/student/dto/schedule-monitoring.dto.ts
+++ src/student/dto/schedule-monitoring.dto.ts
@@ -1,13 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class ScheduleMonitoringDto {
   @ApiProperty({
     example: '2023-01-20 10:00:00',
   })
-  start: string;
+  @Transform(({ value }) => value && new Date(value))
+  start: Date;
 
   @ApiProperty({
     example: '2023-01-20 11:00:00',
   })
-  end: string;
+  @Transform(({ value }) => value && new Date(value))
+  end: Date;
 }
