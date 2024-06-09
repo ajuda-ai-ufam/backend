96c50bee894104c4d2f976bb216318f79444c99a
Jorge Frota <jorgepabloafrota@gmail.com>
2024-06-09 13:47:38 -0400

    chore: add initial vagrant files

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"810ceeef72e8a1ca415a137ca321db7a908579d8ed226c1d43069bc9a306c671","vagrant_version":"2.4.1"}
